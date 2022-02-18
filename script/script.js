7d0209dc9a78f270aa10963cdb76ea81cc81d34c
Xolmurod <softrishton@gmail.com>
2022-02-18 14:06:48 +0500

    JSda yangi muloqotlar

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// "use strict"
+const numberOfNews = prompt('Siz qancha yangilik ko`rdinggiz?');
+const personalNewsDB = {
+    count: numberOfNews,
+    news = {},
+    actors = {},
+    genres = [],
+    privat = false,
+};
+const a = prompt('Oxirgi ko`rgan yangiliklaringizdan biri?');
+const b = prompt('Unga qancha baxo bergan bo`lardinggiz?');
+
+ personalNewsDB.news[a] = b;
+
+ console.log(personalNewsDB);
