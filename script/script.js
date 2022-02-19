a0a887cd6c1dff95231db48c4b908e9bc1e7e572
Xolmurod <softrishton@gmail.com>
2022-02-19 11:54:29 +0500

    oxiri

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 "use strict"
-const numberOfNews = prompt('Siz qancha yangilik ko`rdinggiz?')
+const numberOfNews = +prompt('Siz qancha yangilik ko`rdinggiz?')
 const personalNewsDB = {
     count: numberOfNews,
-    news = {},
-    actors = {},
-    genres = [],
-    privat = false,
-};
+    news: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
 const a = prompt('Oxirgi ko`rgan yangiliklaringizdan biri?')
 const b = prompt('Unga qancha baxo bergan bo`lardinggiz?')
 
